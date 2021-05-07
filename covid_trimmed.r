8a64e708cda43fcfa798252bd48632aec22ac8bd
Mario Antonioletti <mario@epcc.ed.ac.uk>
2021-05-07 11:57:07 +0100

    Enabled the code that reads the R estimate, PredictCases does not need to be removed.

--- covid_trimmed.r
+++ covid_trimmed.r
@@ -38,7 +38,7 @@ baseurl <- "https://api.coronavirus.data.gov.uk/v2/data?"
 
 # Start and end date - the data to collect data from
 startdate <- as.Date("2020/07/25")
-#  To one week ago (-7)
+# To one week ago (-7)
 enddate <-  Sys.Date()-7
 
 # Total cases, deaths, tests
@@ -158,55 +158,55 @@ vacdat <- vacdat %>%
 
 # Get the Government R estimates
 # URL data of where the information is held
-# Rurl <- "https://www.gov.uk/guidance/the-r-value-and-growth-rate"
-#
-# # Get the URL that holds the time series
-# read_html(Rurl) %>% html_nodes(xpath='//a[contains(text(),"time series of published")]') %>%
-#  html_attr("href") -> Rurl
-#
-# # Get the file name from the URL
-# file <- basename(Rurl)
-#
-# # Create a data subdirectory if it does not exist
-# if(!dir.exists("data")){
-#   dir.create("data")
-# }
-#
-# # Download the file with the data if it does not already exist
-# if(!file.exists(paste0("data/",file))){
-#   download.file(Rurl,destfile = paste0("data/",file),quiet = TRUE)
-# }
-#
-# # Read the contents of the file
-# # skip the first 8 rows, table header and merged cells (read can't handle)
-# # read "."s as NAs as the "." is used to mean not applicable
-# Rest <- read_ods(paste0("data/",file), sheet = "Table1_-_R", skip=8, na=".")
-#
-# # Rename the columns
-# names(Rest) <- c("","Date","UK_LowerBound","UK_UpperBound",
-#                  "England_LowerBound","England_UpperBound",
-#                  "EEng_LowerBound","EEng_UpperBound",
-#                  "Lon_LowerBound","Lon_UpperBound","Mid_LowerBound","Mid_UpperBound",
-#                  "NEY_LowerBound","NEY_UpperBound","NW_LowerBound","NW_UpperBound",
-#                  "SE_LowerBound","SE_UpperBound","SW_LowerBound","SW_UpperBound")
-#
-# # Remove the first column that contains nothing
-# Rest <- Rest[,-1]
-#
-# # Convert to a tibble
-# Rest <- as_tibble(Rest)
-#
-# # Convert character dates to dates
-# Rest$Date <- as.Date(Rest$Date, format="%d-%b-%y")
-#
-# # Remove NA values
-# Rest %>% filter(!is.na(Date)) -> Rest
-#
-# # Write the data to a CSV file
-# write_csv(Rest,file="data/R_estimate.csv")
+Rurl <- "https://www.gov.uk/guidance/the-r-value-and-growth-rate"
+
+# Get the URL that holds the time series
+read_html(Rurl) %>% html_nodes(xpath='//a[contains(text(),"time series of published")]') %>%
+ html_attr("href") -> Rurl
+
+# Get the file name from the URL
+file <- basename(Rurl)
+
+# Create a data subdirectory if it does not exist
+if(!dir.exists("data")){
+  dir.create("data")
+}
+
+# Download the file with the data if it does not already exist
+if(!file.exists(paste0("data/",file))){
+  download.file(Rurl,destfile = paste0("data/",file),quiet = TRUE)
+}
+
+# Read the contents of the file
+# skip the first 8 rows, table header and merged cells (read can't handle)
+# read "."s as NAs as the "." is used to mean not applicable
+Rest <- read_ods(paste0("data/",file), sheet = "Table1_-_R", skip=8, na=".")
+
+# Rename the columns
+names(Rest) <- c("","Date","UK_LowerBound","UK_UpperBound",
+                 "England_LowerBound","England_UpperBound",
+                 "EEng_LowerBound","EEng_UpperBound",
+                 "Lon_LowerBound","Lon_UpperBound","Mid_LowerBound","Mid_UpperBound",
+                 "NEY_LowerBound","NEY_UpperBound","NW_LowerBound","NW_UpperBound",
+                 "SE_LowerBound","SE_UpperBound","SW_LowerBound","SW_UpperBound")
+
+# Remove the first column that contains nothing
+Rest <- Rest[,-1]
+
+# Convert to a tibble
+Rest <- as_tibble(Rest)
+
+# Convert character dates to dates
+Rest$Date <- as.Date(Rest$Date, format="%d-%b-%y")
+
+# Remove NA values
+Rest %>% filter(!is.na(Date)) -> Rest
+
+# Write the data to a CSV file
+write_csv(Rest,file="data/R_estimate.csv")
 
 # Read in the data from a csv file
-Rest <- read_csv(file="data/R_estimate.csv")
+#Rest <- read_csv(file="data/R_estimate.csv")
 
 # Plot the UB and LB for the UK R estimates, have added a line commented out
 # where you can plot your estimate for the R value - add your own data frame
