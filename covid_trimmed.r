d0926f724b47afb1f9428137f2eb156d1e40c890
Mario Antonioletti <mario@epcc.ed.ac.uk>
2021-05-11 14:22:18 +0100

    Removed code, separated into a different script.

--- covid_trimmed.r
+++ covid_trimmed.r
@@ -4,7 +4,7 @@
 #
 
 # Copyright 2021 Graeme Ackland, The University of Edinburgh,
-#                James Ackland The University of Cambridge
+#                James Ackland, The University of Cambridge
 
 #
 #### Header ####
@@ -147,7 +147,7 @@ vacurl <- paste0(baseurl,
 
 # Explicitly define the types for the columns
 coltypes <- cols(col_character(), col_character(),col_character(),
-                 col_date(format="%Y-%m-%d"), col_double()) 
+                 col_date(format="%Y-%m-%d"), col_double())
 # Read the data
 vacdat <-  read_csv(file = vacurl, col_types = coltypes)
 
@@ -183,62 +183,9 @@ regdeaths <- regdat %>%  select(date,areaName,
            date <= enddate )%>%
   arrange(date)
 
-#  Use integer areaCodes
-#for (i in 1:length(regdat$areaCode)){
-#regdat$areaCode[i]=as.integer(substr(regdat$areaCode[i],7,9))}
-# Get the Government R estimates
-# URL data of where the information is held
-# Rurl <- "https://www.gov.uk/guidance/the-r-value-and-growth-rate"
-#
-# # Get the URL that holds the time series
-# read_html(Rurl) %>% html_nodes(xpath='//a[contains(text(),"time series of published")]') %>%
-#  html_attr("href") -> Rurl
-#
-# # Get the file name from the URL
-# file <- basename(Rurl)
-#
-# # Create a data subdirectory if it does not exist
-# if(!dir.exists("data")){
-#   dir.create("data")
-# }
-#
-# # Download the file with the data if it does not already exist
-# if(!file.exists(paste0("data/",file))){
-#   download.file(Rurl,destfile = paste0("data/",file),quiet = TRUE)
-# }
-#
-# # Read the contents of the file
-# # skip the first 8 rows, table header and merged cells (read can't handle)
-# # read "."s as NAs as the "." is used to mean not applicable
-# Rest <- read_ods(paste0("data/",file), sheet = "Table1_-_R", skip=8, na=".")
-#
-# # Rename the columns
-# names(Rest) <- c("","Date","UK_LowerBound","UK_UpperBound",
-#                  "England_LowerBound","England_UpperBound",
-#                  "EEng_LowerBound","EEng_UpperBound",
-#                  "Lon_LowerBound","Lon_UpperBound","Mid_LowerBound","Mid_UpperBound",
-#                  "NEY_LowerBound","NEY_UpperBound","NW_LowerBound","NW_UpperBound",
-#                  "SE_LowerBound","SE_UpperBound","SW_LowerBound","SW_UpperBound")
-#
-# # Remove the first column that contains nothing
-# Rest <- Rest[,-1]
-#
-# # Convert to a tibble
-# Rest <- as_tibble(Rest)
-#
-# # Convert character dates to dates
-# Rest$Date <- as.Date(Rest$Date, format="%d-%b-%y")
-#
-# # Remove NA values
-# Rest %>% filter(!is.na(Date)) -> Rest
-#
-# # Write the data to a CSV file
-# write_csv(Rest,file="data/R_estimate.csv")
 
 # Read in the data from a csv file
 Rest <- read_csv(file="data/R_estimate.csv")
-
-
 
 #### Get tests for England pre-Sept by taking the post-Sept fraction of all tests that were in england (0.867)
 
