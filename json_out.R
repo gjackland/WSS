b9d8619367eb507706afef4cb62ac5b55041b20e
Mario Antonioletti <mario@epcc.ed.ac.uk>
2021-06-03 08:23:21 +0100

    Ignore pdf files.

--- json_out.R
+++ json_out.R
@@ -137,7 +137,7 @@ outputJSON <- function(myt0,
                          r0=myr0,
                          calibrationDeathCount=mycalibrationDeathCount,
                          interventionPeriods=myinterventionPeriods
-                         )
+    )
 
     mymetadata <- list(region=myregion,
                        subregion=mysubregion,
@@ -178,9 +178,13 @@ outputJSON <- function(myt0,
     ## Build up the object to be output to JSON
     myobject <- list(time = mytime,
                      metadata = mymetadata,
-                     aggregates = myaggregates)
+                     aggregate = list(metrics=myaggregates))
 
     ## Output to JSON
-    toJSON(myobject,pretty = TRUE,auto_unbox = TRUE, na ="null")
+    if(dir.exists("/data/output")){
+        write_json(myobject, "/data/output/data.json", pretty = TRUE, auto_unbox = TRUE, na ="null")
+    }else{
+        toJSON(myobject,pretty = TRUE,auto_unbox = TRUE, na ="null")
+    }
 
 }
