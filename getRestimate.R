a705429b2d879cf866275c7f919b842678679899
Mario Antonioletti <mario@epcc.ed.ac.uk>
2021-07-15 11:02:06 +0100

    Fixes after putting through lint.

--- getRestimate.R
+++ getRestimate.R
@@ -22,56 +22,59 @@ stop_quietly <- function() {
 
 # Get the Government R estimates
 # URL data of where the information is held
-Rurl <- "https://www.gov.uk/guidance/the-r-value-and-growth-rate"
+r_url <- "https://www.gov.uk/guidance/the-r-value-and-growth-rate"
 
 # Get the URL that holds the time series
-read_html(Rurl) %>% html_nodes(xpath='//a[contains(text(),"time series of published")]') %>%
-  html_attr("href") -> Rurl
+read_html(r_url) %>%
+  html_nodes(xpath = '//a[contains(text(),"time series of published")]') %>%
+  html_attr("href") -> r_url
 
 # Get the file name from the URL
-file <- basename(Rurl)
+file <- basename(r_url)
 
 # Print available data
-message(paste0("\nCurrent data file available: ",file,"."))
+message(paste0("\nCurrent data file available: ", file, "."))
 
 # Create a data subdirectory if it does not exist
-if(!dir.exists("data")){
+if (!dir.exists("data")) {
   dir.create("data")
 }
 
 # Download the file with the data if it does not already exist
-if(!file.exists(paste0("data/uk-data",file))){
-  download.file(Rurl,destfile = paste0("data/uk-data/",file),quiet = TRUE)
+if (!file.exists(paste0("data/uk-data", file))) {
+  download.file(r_url, destfile = paste0("data/uk-data/", file), quiet = TRUE)
 }else{
-  message("Data file already exists locally, not downloading again. Terminating ...\n\n")
+  message("Data file already exists locally, not downloading again.
+          Terminating ...\n\n")
   stop_quietly()
 }
 
 # Read the contents of the file
 # skip the first 8 rows, table header and merged cells (read can't handle)
 # read "."s as NAs as the "." is used to mean not applicable
-Rest <- read_ods(paste0("data/uk-data/",file), sheet = "Table1_-_R", skip=8, na=".")
+r_est <- read_ods(paste0("data/uk-data/", file), sheet = "Table1_-_R",
+                 skip = 8, na = ".")
 
 # Rename the columns
-names(Rest) <- c("","Date","UK_LowerBound","UK_UpperBound",
-                 "England_LowerBound","England_UpperBound",
-                 "EEng_LowerBound","EEng_UpperBound",
-                 "Lon_LowerBound","Lon_UpperBound","Mid_LowerBound","Mid_UpperBound",
-                 "NEY_LowerBound","NEY_UpperBound","NW_LowerBound","NW_UpperBound",
-                 "SE_LowerBound","SE_UpperBound","SW_LowerBound","SW_UpperBound")
+names(r_est) <- c("", "Date", "UK_LowerBound", "UK_UpperBound",
+                 "England_LowerBound", "England_UpperBound",
+                 "EEng_LowerBound", "EEng_UpperBound",
+                 "Lon_LowerBound", "Lon_UpperBound", "Mid_LowerBound",
+                 "Mid_UpperBound", "NEY_LowerBound", "NEY_UpperBound",
+                 "NW_LowerBound", "NW_UpperBound", "SE_LowerBound",
+                 "SE_UpperBound", "SW_LowerBound", "SW_UpperBound")
 
 # Remove the first column that contains nothing
-Rest <- Rest[,-1]
+r_est <- r_est[, -1]
 
 # Convert to a tibble
-Rest <- as_tibble(Rest)
+r_est <- as_tibble(r_est)
 
 # Convert character dates to dates
-Rest$Date <- as.Date(Rest$Date, format="%d-%b-%y")
+r_est$Date <- as.Date(r_est$Date, format = "%d-%b-%y")
 
 # Remove NA values
-Rest %>% filter(!is.na(Date)) -> Rest
+r_est %>% filter(!is.na(Date)) -> r_est
 
 # Write the data to a CSV file
-write_csv(Rest,file="data/R_estimate.csv")
-
+write_csv(r_est, file = "data/R_estimate.csv")
