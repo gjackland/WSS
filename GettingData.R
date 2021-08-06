76f256a4a7e768988d6a82a0a13d515e0198e5d8
Mario Antonioletti <mario@epcc.ed.ac.uk>
2021-08-06 15:44:24 +0100

    Function to create a URL.

--- GettingData.R
+++ GettingData.R
@@ -102,22 +102,70 @@ region7Tocode  <- data.frame(
   "North East and Yorkshire" = "E40000009",
   "North West" = "E40000010"
 )
+
+# 9 to 7 region data mapping assumption:
+#
+# Midlands  = East Midlands + West Midlands
+# E40000008 =  E12000002 + E12000005
+#
+# North East = North East and Yorkshire = North East + Yorkshire and The Humber
+#              E40000009                = E12000001 + E12000003
+#
+# This means that when the Midlands come up then we have to pull data twice and
+# add them up.
+
+
 # Download data -----------------------------------------------------------
 
 # Region to get data for
 subregion <- "East of England"
 
-# Get the code for the subregion
-code <- region9Tocode[subregion]
+# Get the 9 region codes to deal with the special cases
+if (subregion == "Midlands"){
+  code <-  c("E12000002", "E12000005")
+} else if (subregion == "North East"){
+  code <- c("E12000001","E12000003")
+} else {
+  code <- region9Tocode[subregion]
+}
 
-# Check if an English subregion - start with E4
+# Function to return one or more URLs to return the requested data.
+getURL <- function(code, query){
+
+  # Set the url
+  url <-  ""
+
+  # Base URL to get the UK government data
+  baseurl <- "https://api.coronavirus.data.gov.uk/v2/data?"
+
+  # Are we dealing with an English subregion - start with E12
+  isEngSubregion <-  grepl("^E12",code[1])
+
+  if (!isEngSubregion) {
+    header <- paste0("areaType=nation&","areaCode=",code,"&")
+    url <- paste0(baseurl, header, query)
+  } else {
+    for(c in code){
+      header <- paste0("areaType=region&","areaCode=",c,"&")
+      if(url == ""){
+        url <- paste0(baseurl, header, query)
+      } else {
+        url <- c(url, paste0(baseurl, header, query))
+      }
+    }
+  }
+
+  return(url)
+}
+
+# Are we dealing with an English subregion - start with E12
 isEngSubregion <-  grepl("^E12",code)
 
 # Base URL to get the UK government data
 baseurl <- "https://api.coronavirus.data.gov.uk/v2/data?"
 
 # Start and end date for the data
-# for the enddate lose only the last day of data -
+# for the end date lose only the last day of data -
 # use a tail correction for reporting delay
 startdate <- as.Date("2020/07/25")
 enddate <-  Sys.Date()-5
@@ -125,7 +173,7 @@ enddate <-  Sys.Date()-5
 if (!isEngSubregion) { # Dealing with a GB country state
    header <- paste0("areaType=nation&","areaCode=",code,"&")
 } else { # Dealing with an English region
-   header <- paste0("areaType=region&","areaCode=",code,"&") # no data
+   header <- paste0("areaType=region&","areaCode=",code,"&")
 }
 
 # Create URL for total cases, deaths, tests and vaccinations
